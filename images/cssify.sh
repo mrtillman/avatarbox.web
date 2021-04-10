4dda961b9217fdeec19b4a32a4357c6f2b1d8aaa
Stephen Tillman <askmrtillman@gmail.com>
2021-04-10 03:24:08 -0400

    add shell script to convert image to data uri

---
+++ images/cssify.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# https://unix.stackexchange.com/questions/247843/how-to-generate-a-data-uri-from-an-image-file
+mimetype=$(file -bN --mime-type "$1")
+content=$(base64 -w0 < "$1")
+echo "url('data:$mimetype;base64,$content')"
+
+# usage
+# chmod +x ./cssify.sh
+# ./cssify.sh icon.png
